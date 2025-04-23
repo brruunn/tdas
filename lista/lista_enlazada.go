480a288c0456ed6d5dbe35915d33d674b7cc8009
Mumi2004 <tagimenez@fi.uba.ar>
2025-04-23 13:06:20 -0300

    Agrego mis primitivas; falta Iterador. Más algunos cambios generales.

--- lista/lista_enlazada.go
+++ lista/lista_enlazada.go
@@ -12,8 +12,14 @@ type listaEnlazada[T any] struct {
 }
 
 type iterListaEnlazada[T any] struct {
-	// ...
+	anterior *nodoLista[T]
+	actual   *nodoLista[T]
+	lista    *listaEnlazada[T]
 }
+
+const (
+	_MENSAJE_PANIC = "La lista esta vacia"
+)
 
 // Primitivas de lista
 
@@ -21,16 +27,52 @@ func (lista listaEnlazada[T]) EstaVacia() bool {
 	return lista.primero == nil
 }
 
-func (lista listaEnlazada[T]) InsertarPrimero(elemento T) {
+func (lista *listaEnlazada[T]) InsertarPrimero(elemento T) {
 	nuevoNodo := &nodoLista[T]{dato: elemento, siguiente: lista.primero}
 	lista.primero = nuevoNodo
 }
 
+func (lista *listaEnlazada[T]) InsertarUltimo(elemento T) {
+	nuevoNodo := &nodoLista[T]{dato: elemento}
+
+	if lista.EstaVacia() {
+		lista.primero = nuevoNodo
+	} else {
+		lista.ultimo.siguiente = nuevoNodo
+	}
+
+	lista.ultimo = nuevoNodo
+	lista.largo++
+}
+
+func (lista *listaEnlazada[T]) BorrarPrimero() T {
+	primero := lista.VerPrimero()
+
+	if lista.largo == 1 {
+		lista.ultimo = nil
+	}
+
+	lista.primero = lista.primero.siguiente
+	lista.largo--
+	return primero
+}
+
 func (lista listaEnlazada[T]) VerPrimero() T {
 	if lista.EstaVacia() {
-		panic("La lista esta vacia")
+		panic(_MENSAJE_PANIC)
 	}
 	return lista.primero.dato
+}
+
+func (lista listaEnlazada[T]) VerUltimo() T {
+	if lista.EstaVacia() {
+		panic(_MENSAJE_PANIC)
+	}
+	return lista.ultimo.dato
+}
+
+func (lista listaEnlazada[T]) Largo() int {
+	return lista.largo
 }
 
 func (lista listaEnlazada[T]) Iterar(visitar func(T) bool) {
