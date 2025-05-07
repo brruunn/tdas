b55f16f891bd48ac4f00d88fe3f0f0a7eff4675f
Bruno Branger <brunoianbranger@gmail.com>
2025-05-07 01:00:18 -0300

    Obtener(), Borrar() 50%, Iterador() implementados

--- diccionario/abb.go
+++ diccionario/abb.go
@@ -19,7 +19,7 @@ type abb[K comparable, V any] struct {
 	cmp      funcCmp[K]
 }
 
-type iterDiccoonario[K comparable, V any] struct {
+type iterDiccionarioOrdenado[K comparable, V any] struct {
 	pila  TDAPila.Pila[*nodoABB[K, V]]
 	desde *K
 	hasta *K
@@ -44,7 +44,21 @@ func (a *abb[K, V]) Guardar(clave K, dato V) {
 }
 
 func (a *abb[K, V]) Obtener(clave K) V {
-	// ...
+	nodo := a.raiz
+	for nodo != nil {
+		comparacion := a.cmp(clave, nodo.clave)
+		if comparacion == 0 {
+			return nodo.dato // Encontré la clave que estaba buscando
+		}
+		if comparacion < 0 {
+			nodo = nodo.izq
+		}
+		if comparacion > 0 {
+			nodo = nodo.der
+		}
+	}
+	var cero V
+	return cero
 }
 
 func (a *abb[K, V]) Pertenece(clave K) bool {
@@ -52,7 +66,33 @@ func (a *abb[K, V]) Pertenece(clave K) bool {
 }
 
 func (a *abb[K, V]) Borrar(clave K) V {
-	// ...
+	nodo := a.raiz
+	for nodo != nil {
+		comparacion := a.cmp(clave, nodo.clave)
+		if comparacion == 0 {
+			// Caso 1: nodo sin hijos
+			if nodo.izq == nil && nodo.der == nil {
+				// ...
+			}
+			// Caso 2: nodo con un solo hijo
+			if (nodo.izq == nil && nodo.der != nil) || (nodo.izq != nil && nodo.der == nil) {
+				// ...
+			}
+			// Caso 3: nodo con dos hijos
+			if nodo.izq != nil && nodo.der != nil {
+				// ...
+			}
+			break
+		}
+		if comparacion < 0 {
+			nodo = nodo.izq
+		}
+		if comparacion > 0 {
+			nodo = nodo.der
+		}
+	}
+	var cero V
+	return cero
 }
 
 func (a *abb[K, V]) Cantidad() int {
@@ -71,7 +111,19 @@ func (a *abb[K, V]) IterarRango(desde *K, hasta *K, visitar func(K, V) bool) {
 //------------------------------------------------------------------------------------------------------------------------------//
 
 func (a *abb[K, V]) Iterador() IterDiccionario[K, V] {
-	// ...
+	pila := TDAPila.CrearPilaDinamica[*nodoABB[K, V]]()
+	nodo := a.raiz
+
+	for nodo != nil {
+		pila.Apilar(nodo)
+		nodo = nodo.izq
+	}
+
+	return &iterDiccionarioOrdenado[K, V]{
+		pila: pila,
+		cmp:  a.cmp,
+	}
+
 }
 
 func (a *abb[K, V]) IteradorRango(desde *K, hasta *K) IterDiccionario[K, V] {
