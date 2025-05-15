25104583a4a35f6750ca2f11c610663f266bf865
Mumi2004 <tagimenez@fi.uba.ar>
2025-05-14 22:52:14 -0300

    Cambio a un comentario

--- diccionario/hash_aux.go
+++ diccionario/hash_aux.go
@@ -20,7 +20,7 @@ func hashingFNV(clave []byte, tam int) int {
 	return int(h % uint64(tam))
 }
 
-// Auxiliar de buscar y Guardar
+// Auxiliar de hashBuscar
 
 func convertirAPosicion[K comparable](clave K, tam int) int {
 	claveBytes := convertirABytes(clave)
